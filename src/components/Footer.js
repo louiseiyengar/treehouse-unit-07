59ae12e9434646c85ee81cc184f8c295481392d1
Louise Iyengar <lki@louiseiyengar.com>
2019-06-01 14:07:34 -0400

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer =(props) => (
+    <footer className="main-nav">
+      <div>
+          This is my footer
+      </div>
+    </footer>
+);
+
+export default Footer;
